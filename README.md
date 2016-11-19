cfb32caebf6c228f0632664a8a2fc0e0499e275a
Black and Red <wesoly.krzysztofa@gmail.com>
2016-11-19 07:52:32 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,54 @@
+Wolnościowiec Web Proxy
+=======================
+
+Anonymous HTTP proxy that forwards all requests through the PHP application
+on server side.
+
+```
+/*
+ * Wolnościowiec / WebProxy
+ * ------------------------
+ *
+ *   Web Proxy passing through all traffic on port 80
+ *   A part of an anarchist portal - wolnosciowiec.net
+ *
+ *   Wolnościowiec is a project to integrate the movement
+ *   of people who strive to build a society based on
+ *   solidarity, freedom, equality with a respect for
+ *   individual and cooperation of each other.
+ *
+ *   We support human rights, animal rights, feminism,
+ *   anti-capitalism (taking over the production by workers),
+ *   anti-racism, and internationalism. We negate
+ *   the political fight and politicians at all.
+ *
+ *   http://wolnosciowiec.net/en
+ *
+ *   License: LGPLv3
+ */
+````
+
+Installation
+============
+
+```
+export WW_TOKEN="your-api-key-here"
+composer install
+php -S 0.0.0.0:8081 ./index.php
+```
+
+How to use
+==========
+
+Make a request, just as usual. For example POST facebook.com, but move the target url to the header "WW_TARGET_URL"
+and as a URL temporarily set your proxy address.
+
+Example request:
+
+```
+GET http://some-proxy-host:8081/
+
+Headers:
+WW_TARGET_URL: http://facebook.com/ZSP-Związek-Wielobranżowy-Warszawa-290681631074873
+WW_TOKEN: your-api-key-here
+```
---
+++ src/Controllers/PassThroughController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Wolnosciowiec\WebProxy\Controllers;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Psr7\Uri;
+use Proxy\Proxy;
+use Proxy\Adapter\Guzzle\GuzzleAdapter;
+use Zend\Diactoros\ServerRequest;
+use Zend\Diactoros\ServerRequestFactory;
+
+/**
+ * @package Wolnosciowiec\WebProxy\Controllers
+ */
+class PassThroughController
+{
+    /**
+     * @throws \Exception
+     * @return array
+     */
+    private function getRequestedURL()
+    {
+        if (!isset($_SERVER['HTTP_WW_TARGET_URL'])) {
+            throw new \Exception('Request URL not specified. Should be in a header "WW_TARGET_URL"');
+        }
+
+        return $_SERVER['HTTP_WW_TARGET_URL'];
+    }
+
+    /**
+     * @return ServerRequest
+     * @throws \Exception
+     */
+    private function getRequest()
+    {
+        // create a PSR7 request based on the current browser request.
+        $request     = ServerRequestFactory::fromGlobals();
+        $currentHost = $request->getUri()->getHost();
+
+        $requestedUrl = new Uri($this->getRequestedURL());
+        $requestedUrl = $requestedUrl->withPath('');
+
+        /** @var ServerRequest $request */
+        $request = $request->withUri($requestedUrl);
+
+        if ($currentHost === $request->getUri()->getHost()) {
+            throw new \Exception('Cannot make a request to the same host as we are');
+        }
+
+        // do the clean up before passing through the request
+        $request = $request->withoutHeader('ww-target-url');
+        $request = $request->withoutHeader('ww-token');
+
+        return $request;
+    }
+
+    /**
+     * @return Proxy
+     */
+    private function getProxy()
+    {
+        return new Proxy(new GuzzleAdapter(new Client()));
+    }
+
+    /**
+     * @throws \Exception
+     * @return string
+     */
+    public function executeAction()
+    {
+        try {
+            $request = $this->getRequest();
+        }
+        catch (\Exception $e) {
+            $this->sendResponseCode(400);
+            return json_encode([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ]);
+        }
+
+        try {
+            // forward the request and get the response.
+            $response = $this->getProxy()
+                ->forward($request)
+                ->to($this->getRequestedURL());
+        }
+        catch (ConnectException $e) {
+            $this->sendResponseCode(400);
+            return json_encode([
+                'success' => false,
+                'message' => 'Connection error',
+                'details' => $e->getMessage(),
+            ]);
+        }
+        catch (ClientException $e) {
+            $this->sendResponseCode(404);
+            return json_encode([
+                'success' => false,
+                'message' => 'Got an ClientException',
+                'details' => $e->getMessage(),
+                'body'    => $e->getResponse()->getBody()->getContents(),
+            ]);
+        }
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @param string $code
+     * @throws \InvalidArgumentException
+     */
+    public function sendResponseCode($code)
+    {
+        if (defined('IS_EMULATED_ENVIRONMENT') && IS_EMULATED_ENVIRONMENT) {
+            return;
+        }
+
+        if ($code === 404) {
+            header('HTTP/1.0 404 Not Found');
+            return;
+        }
+        elseif ($code === 403) {
+            header('HTTP 1.1 403 Unauthorized');
+            return;
+        }
+        elseif ($code === 400) {
+            header('HTTP/1.0 400 Bad Request');
+            return;
+        }
+
+        throw new \InvalidArgumentException('Unrecognized code passed');
+    }
+}
